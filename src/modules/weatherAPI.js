b37c2be81e1ba1ae49084f2b39589069ae66aab2
oscarleonnogales <oscarleonnogales@gmail.com>
2021-01-14 01:04:13 -0800

    Changed error message

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -6,6 +6,6 @@ export async function getWeatherData(location) {
 		const data = await response.json();
 		return data;
 	} catch (err) {
-		console.log(`Error: ${err}`);
+		console.error(`Error: ${err}`);
 	}
 }
