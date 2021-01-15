892976f1713e3ef65bca494af8a0c3a6475049ae
oscarleonnogales <oscarleonnogales@gmail.com>
2021-01-14 22:00:26 -0800

    Changed to HTTPS

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -2,7 +2,7 @@ const API_KEY = '1986480656ec490d950204923202611';
 
 export async function getWeatherData(location) {
 	try {
-		const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}`);
+		const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}`);
 		const data = await response.json();
 		return data;
 	} catch (err) {
